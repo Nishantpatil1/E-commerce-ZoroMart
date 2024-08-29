78ac776e84b097b59dcfeb510a41dba23ba06f3f
Nishantpatil1 <159808034+Nishantpatil1@users.noreply.github.com>
2024-08-29 18:59:26 +0530

    Update product.js

--- src/product.js
+++ src/product.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connect =mongoose.connect("mongodb://localhost:27017/login");
+const connect =mongoose.connect("mongodb://##############");
 
 const productSchema = new mongoose.Schema({
 
