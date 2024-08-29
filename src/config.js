e8f69f7cc40d367f5f77ba108588dcd070e6dd15
Nishantpatil1 <159808034+Nishantpatil1@users.noreply.github.com>
2024-08-29 18:59:00 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connect =mongoose.connect("mongodb://localhost:27017/login");
+const connect =mongoose.connect("mongodb://################");
 
 connect.then(()=>{
     console.log("Database connected successfully");
